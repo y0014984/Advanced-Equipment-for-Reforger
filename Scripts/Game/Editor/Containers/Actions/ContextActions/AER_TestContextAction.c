8650dd31c800d1a9e405b2bf061858ffe993103a
y0014984 <y0014984@gmail.com>
2023-09-12 16:56:15 +0200

    added basic Game Master context action

---
+++ Scripts/Game/Editor/Containers/Actions/ContextActions/AER_TestContextAction.c
@@ -0,0 +1,18 @@
+[BaseContainerProps(), SCR_BaseContainerCustomTitleUIInfo("m_Info")]
+class AER_TestContextAction : SCR_GeneralContextAction
+{
+	override bool CanBeShown(SCR_EditableEntityComponent hoveredEntity, notnull set<SCR_EditableEntityComponent> selectedEntities, vector cursorWorldPosition, int flags)
+	{
+		return true;
+	}
+	
+	override bool CanBePerformed(SCR_EditableEntityComponent hoveredEntity, notnull set<SCR_EditableEntityComponent> selectedEntities, vector cursorWorldPosition, int flags)
+	{
+		return true;
+	}
+	
+	override void Perform(SCR_EditableEntityComponent hoveredEntity, notnull set<SCR_EditableEntityComponent> selectedEntities, vector cursorWorldPosition,int flags, int param = -1)
+	{
+		Print("AER success: Game Master Context Action");
+	}
+}
