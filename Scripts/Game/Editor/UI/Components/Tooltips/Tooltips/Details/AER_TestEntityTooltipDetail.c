771caa9c6a2a56ce7dce7efd278b2bd9de2ebf78
y0014984 <y0014984@gmail.com>
2023-09-13 09:56:39 +0200

    added rich text testing widget

---
+++ Scripts/Game/Editor/UI/Components/Tooltips/Tooltips/Details/AER_TestEntityTooltipDetail.c
@@ -0,0 +1,11 @@
+class AER_TestEntityTooltipDetail : SCR_EntityTooltipDetail
+{
+	override bool InitDetail(SCR_EditableEntityComponent entity, Widget widget)
+	{}
+	
+	override bool NeedUpdate()
+	{}
+	
+	override void UpdateDetail(SCR_EditableEntityComponent entity)
+	{}
+}
